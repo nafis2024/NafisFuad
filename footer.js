23c38cb111f85e18483ac7ffdc21b47df757a4a9
Mohammad Nafis Fuad <166198863+nafis2024@users.noreply.github.com>
2025-02-25 12:59:22 +0000

    Final update tuesday

---
+++ footer.js
@@ -0,0 +1,29 @@
+const github = document.getElementById('button1');
+
+github.onclick = function() {
+    window.location.href = 'https://github.com/nafis2024';
+};
+
+const linkedin = document.getElementById('button2');
+
+linkedin.onclick = function() {
+    window.location.href = 'https://linkedin.com/in/nafis2024';
+};
+
+const whatsapp= document.getElementById('button3');
+
+whatsapp.onclick = function() {
+    window.location.href = 'https://wa.me/8801967517101';
+};
+
+const messenger = document.getElementById('button4');
+
+messenger.onclick = function() {
+    window.location.href = 'https://m.me/mn.fuad26';
+};
+
+const mail = document.getElementById('button5');
+
+mail.onclick = function() {
+    window.location.href = 'mailto:nafisfuad2024@gmail.com';
+};
